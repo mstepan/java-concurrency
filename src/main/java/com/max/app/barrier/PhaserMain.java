c36dc99608ffd167a28297147fa060a8e4cf3d54
Maksym Stepanenko <maksym.stepanenko@opower.com>
2019-05-20 12:26:14 +0300

    Phaser modified

--- src/main/java/com/max/app/barrier/PhaserMain.java
+++ src/main/java/com/max/app/barrier/PhaserMain.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Phaser;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PhaserMain {
 
@@ -17,19 +18,26 @@ public class PhaserMain {
 
         final ExecutorService pool = Executors.newCachedThreadPool();
 
+        final AtomicBoolean completed = new AtomicBoolean(false);
+
         final Phaser phaser = new Phaser();
         phaser.register();
 
+
         for (int i = 0; i < 3; ++i) {
-            pool.submit(new Task(phaser));
+            pool.submit(new Task(phaser, completed));
         }
 
-        final int prevPhase = phaser.getPhase();
-        LOG.info("main phase {}", prevPhase);
-
+        LOG.info("main phase {}", phaser.getPhase());
         phaser.arriveAndAwaitAdvance();
 
-        LOG.info("main completed phase {}", prevPhase);
+        LOG.info("main phase {}", phaser.getPhase());
+        phaser.arriveAndAwaitAdvance();
+
+        completed.set(true);
+
+        LOG.info("main phase {}", phaser.getPhase());
+        phaser.arriveAndAwaitAdvance();
 
         phaser.arriveAndDeregister();
 
@@ -42,32 +50,37 @@ public class PhaserMain {
     private static final class Task implements Runnable {
 
         private final Phaser phaser;
+        private final AtomicBoolean completed;
 
-        Task(Phaser phaser) {
+        Task(Phaser phaser, AtomicBoolean completed) {
             this.phaser = phaser;
+            this.completed = completed;
             this.phaser.register();
         }
 
         @Override
         public void run() {
-            try {
-                TimeUnit.SECONDS.sleep(1);
+            while (!(completed.get() || phaser.isTerminated())) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
 
-                LOG.info("start thread {} phase {}", Thread.currentThread().getId(), phaser.getPhase());
+                    LOG.info("start thread {} phase {}", Thread.currentThread().getId(), phaser.getPhase());
 
-                TimeUnit.SECONDS.sleep(1);
+                    TimeUnit.SECONDS.sleep(1);
+                }
+                catch (InterruptedException interEx) {
+                    Thread.currentThread().interrupt();
+                    LOG.info(interEx.getMessage(), interEx);
+                }
+                finally {
+                    LOG.info("end thread {} phase {}", Thread.currentThread().getId(), phaser.getPhase());
+                    phaser.arriveAndAwaitAdvance();
+                }
             }
-            catch (InterruptedException interEx) {
-                Thread.currentThread().interrupt();
-                LOG.info(interEx.getMessage(), interEx);
-            }
-            finally {
-                LOG.info("end thread {} phase {}", Thread.currentThread().getId(), phaser.getPhase());
-                phaser.arriveAndDeregister();
-            }
+
+            phaser.arriveAndDeregister();
         }
     }
-
 
     public static void main(String[] args) {
         try {
